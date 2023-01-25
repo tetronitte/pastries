adc68ba4488327d74f67754e7a9d63b3449f4112
tetronitte <68910823+tetronitte@users.noreply.github.com>
2023-01-25 10:46:37 +0100

    PadtryServlet.java

---
+++ src/main/java/com/example/pastries_jee/servlet/PastryServlet.java
@@ -0,0 +1,28 @@
+package com.example.pastries_jee.servlet;
+
+import com.example.dao.FactoryDao;
+import com.example.entities.Pastry;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(urlPatterns = "/pastry/*")
+public class PastryServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getPathInfo();
+        Optional<Pastry> pastry = FactoryDao.getPastryDao().getByName(name.substring(1));
+        if (pastry.isPresent()) {
+            request.setAttribute("pastry", pastry.get());
+            request.getRequestDispatcher("/WEB-INF/pastry.jsp").forward(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "La pâtisserie demandée est introuvable");
+        }
+    }
+}
